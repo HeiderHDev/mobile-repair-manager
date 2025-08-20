768bc544c084b52b5ae32b8fef4e6f5fcc70b976
HeiderHDev <heiderhrnndz@gmail.com>
2025-08-19 23:35:00 -0500

    feat: create initial user management components and structure

---
+++ src/app/features/users/components/user-form-modal/user-form-modal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-form-modal',
+  imports: [],
+  templateUrl: './user-form-modal.html',
+  styles: ``
+})
+export class UserFormModal {
+
+}
---
+++ src/app/features/users/users-list/users-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-list',
+  imports: [],
+  templateUrl: './users-list.html',
+  styles: ``
+})
+export class UsersList {
+
+}
